8ea4d2e64ace3e7df09087b537a8f07cbbd2b7b9
thisa93 <thisa93@gmail.com>
2025-10-27 16:46:10 +0530

    Final Code Updated

--- Introduction/6AsyncFetchAPI.js
+++ Introduction/6AsyncFetchAPI.js
@@ -3,10 +3,12 @@ async function fetchUsers() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await response.json();
     //users.forEach(user => console.log(user));
-    //console.log(JSON.stringify(users, null, 2));
-    console.log(users);
+    console.log(JSON.stringify(users, null, 3));
+    //console.log(users);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
+
+JSON.stringify()
 fetchUsers();
